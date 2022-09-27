b15e2a888386264b50c26d215a97237a9dae44a6
Amanda Andrade <57667417+aandradebio@users.noreply.github.com>
2022-09-27 15:15:17 -0300

    Update mnmer.R

--- R/mnmer.R
+++ R/mnmer.R
@@ -1,7 +1,17 @@
-mnmer <- function (file, k, m)
-{
-	ctab <- .Call ("cmnmer", file, k, m)
-	tab <- read.csv (text=ctab, header=T)
-
-	return (tab)
+mnmer <- function (file, k, m) {
+  if (!file.exists(file)) {
+    stop("The file does not exists")}
+  
+  k <- as.integer(k)
+  m <- as.integer(m)
+  
+  if (typeof(k) !="integer" || typeof(m)!="integer" || k =="0" || m > k){
+    stop("Fix the parameters")
+  }
+  
+    ctab <- .Call("cmnmer", file, k, m)
+    tab <- read.csv(text = ctab, header = T)
+    return(tab)
 }
+
+
